979720553dcc39b7baf9c8ea1b5821def0f0a90c
Jan <janjus2004@gmail.com>
2025-05-19 17:07:52 +0200

    Small mistake while copying. now it is the good colde in this file :)

--- Organism.cpp
+++ Organism.cpp
@@ -1,92 +1,50 @@
-#include <iostream>
-#include "Position.h"
 #include "Organism.h"
-#include "Plant.h"
-#include "Animal.h"
-#include "World.h"
 
-using namespace std;
-
-int main()
+Organism::Organism(int power, Position position)
 {
-	// Position 
-	Position p1;
-	Position p2{ 1, 1 };
-	Position p3{ -3, -5 };
+	setPower(power);
+	setPosition(position);
+	setSpecies("O");
+}
 
-	//cout << p1.toString() << endl;
-	//cout << p2.toString() << endl;
-	//cout << p3.toString() << endl;
-	//cout << p1.distance(p2) << endl;
-	//p2.move(4, 6);
-	//cout << p2.toString() << endl;
+int Organism::getPower()
+{
+	return this->power;
+}
 
-	// Organism
-	//Organism org1;
-	//Organism org2{ 10, p2 };
+void Organism::setPower(int power)
+{
+	this->power = power;
+}
 
-	//cout << org1.toString() << endl;
-	//cout << org2.toString() << endl;
-	//org1.move(2, 3);
-	//cout << org1.toString() << endl;
+Position Organism::getPosition()
+{
+	return this->position;
+}
 
-	// Plant & Animal
-	Plant plant{ 3, p3 };
-	Animal animal{ 5, p2 };
-	Plant plant2;
-	Animal animal2;
+void Organism::setPosition(Position position)
+{
+	this->position = position;
+}
 
-	cout << plant.toString() << endl;
-	cout << animal.toString() << endl;
-	cout << plant2.toString() << endl;
-	cout << animal2.toString() << endl;
-	plant.move(3, 4);
-	cout << plant.toString() << endl;
-	animal.move(1, 2);
-	cout << animal.toString() << endl;
-	
-	// World test
-	World world;
-	Position posP1{ 4, 5 };
-	Plant plantW1{ 3, posP1 };
-	Position posP2{ 5, 4 };
-	Plant plantW2{ 3, posP2 };
+string Organism::toString()
+{
+	return "{ species: " + this->getSpecies() + 
+		", power: " + to_string(getPower()) + 
+		", position: " + getPosition().toString() + "}";
+}
 
-	Position posW2{ 3, 2 };
-	Animal animalW1{ 6, posW2 };
-	Position posW3{ 2, 3 };
-	Animal animalW2{ 6, posW3 };
+void Organism::move(int dx, int dy)
+{
+	position.move(dx, dy);
+}
 
-	world.addOrganism(&plantW1);
-	world.addOrganism(&plantW2);
-	world.addOrganism(&animalW1);
-	world.addOrganism(&animalW2);
+string Organism::getSpecies()
+{
+	return this->species;
+}
 
-	auto positions = world.getVectorOfFreePositionsAround(Position(5, 5));
-
-	for(auto pos: positions)
-		cout << pos.toString() << endl;
-
-	// Tura 0
-	cout << world.toString() << endl;
-
-	// Tura 1
-	world.makeTurn();
-	cout << world.toString() << endl;
-
-	// Tura 2
-	world.makeTurn();
-	cout << world.toString() << endl;
-
-	world.writeWorld("world.bin");
-
-	// Tura 3
-	world.makeTurn();
-	cout << world.toString() << endl;
-
-	// powrot do Tury 2
-	world.readWorld("world.bin");
-	cout << world.toString() << endl;
-
-	return 0;
+void Organism::setSpecies(string spec)
+{
+	this->species = spec;
 }
--- Organism.h
+++ Organism.h
@@ -1,92 +1,28 @@
-#include <iostream>
+#pragma once
+#include <string>
 #include "Position.h"
-#include "Organism.h"
-#include "Plant.h"
-#include "Animal.h"
-#include "World.h"
 
 using namespace std;
 
-int main()
+class Organism
 {
-	// Position 
-	Position p1;
-	Position p2{ 1, 1 };
-	Position p3{ -3, -5 };
+private:
+	int power;
+	Position position;
+	string species;
+public:
+	Organism(int power, Position position);
+	Organism() : power(0), position(0, 0), species("O") {};
 
-	//cout << p1.toString() << endl;
-	//cout << p2.toString() << endl;
-	//cout << p3.toString() << endl;
-	//cout << p1.distance(p2) << endl;
-	//p2.move(4, 6);
-	//cout << p2.toString() << endl;
+	int getPower();
+	void setPower(int power);
+	Position getPosition();
+	void setPosition(Position position);
+	string getSpecies();
+	void setSpecies(string spec);
 
-	// Organism
-	//Organism org1;
-	//Organism org2{ 10, p2 };
+	string toString();
 
-	//cout << org1.toString() << endl;
-	//cout << org2.toString() << endl;
-	//org1.move(2, 3);
-	//cout << org1.toString() << endl;
+	virtual void move(int dx, int dy);
 
-	// Plant & Animal
-	Plant plant{ 3, p3 };
-	Animal animal{ 5, p2 };
-	Plant plant2;
-	Animal animal2;
-
-	cout << plant.toString() << endl;
-	cout << animal.toString() << endl;
-	cout << plant2.toString() << endl;
-	cout << animal2.toString() << endl;
-	plant.move(3, 4);
-	cout << plant.toString() << endl;
-	animal.move(1, 2);
-	cout << animal.toString() << endl;
-	
-	// World test
-	World world;
-	Position posP1{ 4, 5 };
-	Plant plantW1{ 3, posP1 };
-	Position posP2{ 5, 4 };
-	Plant plantW2{ 3, posP2 };
-
-	Position posW2{ 3, 2 };
-	Animal animalW1{ 6, posW2 };
-	Position posW3{ 2, 3 };
-	Animal animalW2{ 6, posW3 };
-
-	world.addOrganism(&plantW1);
-	world.addOrganism(&plantW2);
-	world.addOrganism(&animalW1);
-	world.addOrganism(&animalW2);
-
-	auto positions = world.getVectorOfFreePositionsAround(Position(5, 5));
-
-	for(auto pos: positions)
-		cout << pos.toString() << endl;
-
-	// Tura 0
-	cout << world.toString() << endl;
-
-	// Tura 1
-	world.makeTurn();
-	cout << world.toString() << endl;
-
-	// Tura 2
-	world.makeTurn();
-	cout << world.toString() << endl;
-
-	world.writeWorld("world.bin");
-
-	// Tura 3
-	world.makeTurn();
-	cout << world.toString() << endl;
-
-	// powrot do Tury 2
-	world.readWorld("world.bin");
-	cout << world.toString() << endl;
-
-	return 0;
-}
+};
